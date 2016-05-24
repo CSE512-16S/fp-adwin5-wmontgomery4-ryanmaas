e651e95cc6ffac40cb60365e7dd3028f1aa8f5f2
Harley Montgomery <harleymontgomery@gmail.com>
2016-05-24 14:33:28 -0700

    fix whitespace

--- script.js
+++ script.js
@@ -16,25 +16,25 @@ var svg = d3.select("body").append("svg")
 file = 'bodypart_full/body_part_json_trial_10/0302.json'
 d3.json(file, function(error, root) {
     if (error) throw error;
-  
+
     var nodes = tree.nodes(root),
         links = tree.links(nodes);
-  
+
     var link = svg.selectAll(".link")
         .data(links)
       .enter().append("path")
         .attr("class", "link")
         .attr("d", diagonal);
-  
+
     var node = svg.selectAll(".node")
         .data(nodes)
       .enter().append("g")
         .attr("class", "node")
         .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; });
-  
+
     node.append("circle")
         .attr("r", 4.5);
-  
+
     node.append("text")
         .attr("dy", ".31em")
         .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
