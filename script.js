f6d50f095fa30e8c15bdae5769b44630b1199bcd
Harley Montgomery <harleymontgomery@gmail.com>
2016-05-26 12:45:14 -0700

    wip, edge highlighting almost there

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 var diameter = 960;
 
-var tree = d3.layout.tree()
-    .size([360, diameter / 2 - 120])
-    .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+// Globals we'll manipulate
+// TODO: better way to handle this?
+nodes = null;
+links = null;
+hiddenLinks = null;
+weightMatrix = null;
 
+// File paths
+tree_json = 'bodypart_full/body_part_json_trial_10/0302.json';
+//edges_txt = 'bodypart_full/edge_prob_bodypart_trial_10.txt';
+edges_txt = 'last_edges.csv';
+
+// Tree layout is only used for manipulating json trees
+var tree = d3.layout.tree()
+
+// Actual layout comes from force layout
 var force = d3.layout.force()
-    .charge(-200)
+    .charge(-150)
     .linkDistance(80)
     .size([diameter, diameter])
 
+// Create the svg canvas into which everything goes
 var svg = d3.select("body").append("svg")
     .attr("width", diameter)
     .attr("height", diameter)
@@ -17,51 +30,67 @@ function highlightNode(d) {
     // Don't highlight entity
     if (d.index === 0) return;
 
+    // Highlight the node
     d3.select(this).select("circle")
         .style("fill", "#ff0");
 
-    hiddenLinks = [{source: nodes[this.index], target: nodes[0]}];
-    d3.selectAll(".node.hidden")
-        .data(links)
-      .enter().append("line")
-        .attr("class", "link")
-        .attr("class", "hidden");
+    var weights = weightMatrix[d.index - 1];
+
+    hiddenLinks = [];
+    for (var i in weights){
+        if (weights.hasOwnProperty(i) && weights[i] > 1e-3) {
+            hiddenLinks.push({
+                source: nodes[i],
+                target: nodes[d.index],
+            });
+        }
+    }
+
+    console.log(weights);
+    console.log(hiddenLinks);
+//    weights = weights.filter(function (d) { return d > 1e-3; });
+//    hiddenLinks = [{source: nodes[d.index], target: nodes[0]}];
+
+
+    svg.selectAll(".link.hidden")
+        .data(hiddenLinks)
+      .enter().insert("line", ":first-child")
+        .attr("class", "link hidden")
+        .attr("stroke", "#c11");
 
     force
-        .links(links.concat(hiddenLinks));
+        .nodes(nodes)
+        .links(links.concat(hiddenLinks))
+        .start();
 }
 
 function unHighlightNode(d) {
+    // TODO: why doesn't svg.select work??
     d3.select(this).select("circle")
       .style("fill", "#fff");
 
-    d3.selectAll(".link.hidden")
+    svg.selectAll(".link.hidden")
         .remove();
     
     force
-        .links(links);
+        .nodes(nodes)
+        .links(links)
+        .start();
 }
 
 function drawGraph() {
-    d3.selectAll(".link")
+    svg.selectAll(".link")
         .attr("x1", function(d) { return d.source.x; })
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
 
-    d3.selectAll(".node")
+    svg.selectAll(".node")
         .attr("transform", function(d) {
             return "translate(" + d.x + " " + d.y + ")";
         });
 }
 
-tree_json = 'bodypart_full/body_part_json_trial_10/0302.json';
-//edges_txt = 'bodypart_full/edge_prob_bodypart_trial_10.txt';
-edges_txt = 'last_edges.csv';
-
-
-nodes = null;
-links = null;
 d3.json(tree_json, function(error, root) {
     if (error) throw error;
 
@@ -76,7 +105,8 @@ d3.json(tree_json, function(error, root) {
     var link = svg.selectAll(".link")
         .data(links)
       .enter().append("line")
-        .attr("class", "link");
+        .attr("class", "link")
+        .attr("stroke", "#ccc");
 
     var node = svg.selectAll(".node")
         .data(nodes)
@@ -103,11 +133,8 @@ d3.json(tree_json, function(error, root) {
     force.on("tick", drawGraph);
 });
 
-/*
-weightMatrix = null;
-d3.csv(edges_txt, function(error, edges_data) {
+d3.csv(edges_txt, function(error, edges) {
     if (error) throw error;
 
-    weightMatrix = edges_data;
+    weightMatrix = edges;
 });
-*/
