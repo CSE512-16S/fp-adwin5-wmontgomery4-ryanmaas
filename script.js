f106217cef357db9302e3292f0698030e96165d3
Harley Montgomery <harleymontgomery@gmail.com>
2016-05-26 10:52:46 -0700

    wip

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var tree = d3.layout.tree()
     .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
 
 var force = d3.layout.force()
-    .charge(-120)
+    .charge(-200)
     .linkDistance(80)
     .size([diameter, diameter])
 
@@ -14,26 +14,59 @@ var svg = d3.select("body").append("svg")
     .attr("height", diameter)
 
 function highlightNode(d) {
+    // Don't highlight entity
+    if (d.index === 0) return;
+
     d3.select(this).select("circle")
         .style("fill", "#ff0");
 
-    d3.select(this).selectAll(".hidden")
-    console.log(d);
+    hiddenLinks = [{source: nodes[this.index], target: nodes[0]}];
+    d3.selectAll(".node.hidden")
+        .data(links)
+      .enter().append("line")
+        .attr("class", "link")
+        .attr("class", "hidden");
+
+    force
+        .links(links.concat(hiddenLinks));
 }
 
 function unHighlightNode(d) {
     d3.select(this).select("circle")
       .style("fill", "#fff");
+
+    d3.selectAll(".link.hidden")
+        .remove();
+    
+    force
+        .links(links);
+}
+
+function drawGraph() {
+    d3.selectAll(".link")
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    d3.selectAll(".node")
+        .attr("transform", function(d) {
+            return "translate(" + d.x + " " + d.y + ")";
+        });
 }
 
 tree_json = 'bodypart_full/body_part_json_trial_10/0302.json';
 //edges_txt = 'bodypart_full/edge_prob_bodypart_trial_10.txt';
 edges_txt = 'last_edges.csv';
+
+
+nodes = null;
+links = null;
 d3.json(tree_json, function(error, root) {
     if (error) throw error;
 
-    var nodes = tree.nodes(root),
-        links = tree.links(nodes);
+    nodes = tree.nodes(root),
+    links = tree.links(nodes);
 
     force
         .nodes(nodes)
@@ -67,24 +100,14 @@ d3.json(tree_json, function(error, root) {
     node.on("mouseover", highlightNode)
         .on("mouseout", unHighlightNode);
 
-    force.on("tick", function() {
-        link.attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
-
-        node.attr("transform", function(d) {
-            return "translate(" + d.x + " " + d.y + ")";
-        })
-    });
+    force.on("tick", drawGraph);
 });
 
 /*
-edges = null;
+weightMatrix = null;
 d3.csv(edges_txt, function(error, edges_data) {
     if (error) throw error;
 
-    edges = edges_data;
-    console.log(edges);
+    weightMatrix = edges_data;
 });
 */
