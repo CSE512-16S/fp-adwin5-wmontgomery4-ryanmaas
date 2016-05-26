b9f8207a90631acf8ff050622c51509d30408879
Harley Montgomery <harleymontgomery@gmail.com>
2016-05-26 15:33:12 -0700

    in working state, needs some updating though

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 var diameter = 960;
 
+// Constants
+WEIGHT_THRESHOLD = 5e-2; // Show hidden edges stronger than this
+LINK_WIDTH = 5; // Base stroke width for links
+
 // Globals we'll manipulate
 // TODO: better way to handle this?
-nodes = null;
-links = null;
-hiddenLinks = null;
-weightMatrix = null;
+var nodes;
+var links;
+var names;
+var hiddenLinks;
+var weightMatrix;
 
 // File paths
-tree_json = 'bodypart_full/body_part_json_trial_10/0302.json';
-//edges_txt = 'bodypart_full/edge_prob_bodypart_trial_10.txt';
-edges_txt = 'last_edges.csv';
+var tree_json = 'bodypart_full/body_part_json_trial_10/0302.json';
+//var edges_txt = 'bodypart_full/edge_prob_bodypart_trial_10.txt';
+var weights_csv = 'last_edges.csv';
+var names_txt = 'bodypart_full/list_of_words_bodypart.txt';
+
 
 // Tree layout is only used for manipulating json trees
 var tree = d3.layout.tree()
@@ -19,7 +26,8 @@ var tree = d3.layout.tree()
 var force = d3.layout.force()
     .charge(-150)
     .linkDistance(80)
-    .size([diameter, diameter])
+    .linkStrength(link => link.strength || 1.0)
+    .size([diameter, diameter]);
 
 // Create the svg canvas into which everything goes
 var svg = d3.select("body").append("svg")
@@ -27,39 +35,47 @@ var svg = d3.select("body").append("svg")
     .attr("height", diameter)
 
 function highlightNode(d) {
-    // Don't highlight entity
+    // Don't highlight 'entity'
     if (d.index === 0) return;
 
     // Highlight the node
     d3.select(this).select("circle")
         .style("fill", "#ff0");
 
-    var weights = weightMatrix[d.index - 1];
-
+    // Set up the new hiddenLinks
+    // NOTE: this requires some tricky indexing because the ordering
+    //       of nodes in the tree differs from the alphabetical order
     hiddenLinks = [];
-    for (var i in weights){
-        if (weights.hasOwnProperty(i) && weights[i] > 1e-3) {
+    var nameIndex = names.indexOf(d.name);
+    var weights = weightMatrix[nameIndex];
+    for (var i in weights){ // loops over the name indices
+        if (weights.hasOwnProperty(i) && weights[i] > WEIGHT_THRESHOLD) {
+            // Annoying indexing stuff, because 'entity' isn't in names
+            // TODO: make this nicer by munging data files better
+            if (i > 0) {
+                var sourceNode = nodes.find(node => node.name === names[i-1]);
+            } else {
+                var sourceNode = nodes[0];
+            }
+
             hiddenLinks.push({
-                source: nodes[i],
-                target: nodes[d.index],
+                source: sourceNode,
+                target: nodes[d.index], // DONT use nameIndex
+                strength: weights[i],
             });
         }
     }
 
-    console.log(weights);
-    console.log(hiddenLinks);
-//    weights = weights.filter(function (d) { return d > 1e-3; });
-//    hiddenLinks = [{source: nodes[d.index], target: nodes[0]}];
-
-
+    // Update the svg/force
     svg.selectAll(".link.hidden")
         .data(hiddenLinks)
-      .enter().insert("line", ":first-child")
+//      .enter().insert("line", ":first-child")
+      .enter().append("line")
         .attr("class", "link hidden")
-        .attr("stroke", "#c11");
+        .attr("stroke", "#c11")
+        .attr("stroke-width", d => LINK_WIDTH * d.strength);
 
     force
-        .nodes(nodes)
         .links(links.concat(hiddenLinks))
         .start();
 }
@@ -73,30 +89,37 @@ function unHighlightNode(d) {
         .remove();
     
     force
-        .nodes(nodes)
         .links(links)
         .start();
 }
 
 function drawGraph() {
     svg.selectAll(".link")
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
 
     svg.selectAll(".node")
-        .attr("transform", function(d) {
-            return "translate(" + d.x + " " + d.y + ")";
-        });
+        .attr("transform", d => "translate(" + d.x + " " + d.y + ")");
 }
 
-d3.json(tree_json, function(error, root) {
+queue()
+    .defer(d3.json, tree_json)
+    .defer(d3.csv, weights_csv)
+    .defer(d3.text, names_txt)
+    .await(setup);
+
+function setup(error, root, weight_data, name_data) {
     if (error) throw error;
 
+    // Store stuff
     nodes = tree.nodes(root),
     links = tree.links(nodes);
+    weightMatrix = weight_data;
+    names = name_data.split('\n');
 
+    // Set up the force layout
     force
         .nodes(nodes)
         .links(links)
@@ -106,7 +129,8 @@ d3.json(tree_json, function(error, root) {
         .data(links)
       .enter().append("line")
         .attr("class", "link")
-        .attr("stroke", "#ccc");
+        .attr("stroke", "#ccc")
+        .attr("stroke-width", LINK_WIDTH);
 
     var node = svg.selectAll(".node")
         .data(nodes)
@@ -115,7 +139,7 @@ d3.json(tree_json, function(error, root) {
         .call(force.drag);
 
     node.append("circle")
-        .attr("r", function(d) { return 60 / (d.depth + 2); });
+        .attr("r", d => 60 / (d.depth + 2));
         /*
         .attr("r", function(d) {
             return d.children ? 10*Math.sqrt(d.children.length+1) : 10;
@@ -125,16 +149,10 @@ d3.json(tree_json, function(error, root) {
     node.append("text")
         .attr("dy", ".31em")
         .attr("text-anchor", "middle")
-        .text(function(d) { return d.name; });
+        .text(d => d.name);
 
     node.on("mouseover", highlightNode)
         .on("mouseout", unHighlightNode);
 
     force.on("tick", drawGraph);
-});
-
-d3.csv(edges_txt, function(error, edges) {
-    if (error) throw error;
-
-    weightMatrix = edges;
-});
+}
